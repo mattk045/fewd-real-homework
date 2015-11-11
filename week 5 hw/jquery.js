3c675c63915dbdbd2c2b98da4fdd6d976d35b8b6
mattk045 <kennedy.mjh@gmail.com>
2015-11-11 17:19:27 -0500

    week 5 hw fix

--- week 5 hw/jquery.js
+++ week 5 hw/jquery.js
@@ -9,27 +9,27 @@ cityValue = cityValue.toLowerCase().trim();
 
 if(cityValue == "new york" || cityValue == "nyc" || cityValue == "new york city"){
 $("body").attr('class', 'nyc');
-
+document.getElementById("test").reset();
 }
 
 if(cityValue == "san francisco" || cityValue == "sf" || cityValue == "bay area"){
 $("body").attr('class', 'sf');
-
+document.getElementById("test").reset();
 }
 
 if(cityValue == "los angeles" || cityValue == "la" || cityValue == "lax"){
 $("body").attr('class', 'la');
-
+document.getElementById("test").reset();
 }
 
 if(cityValue == "austin" || cityValue == "atx"){
 $("body").attr('class', 'austin');
-
+document.getElementById("test").reset();
 }
 
 if(cityValue == "sydney" || cityValue == "syd"){
 $("body").attr('class', 'sydney');
-
+document.getElementById("test").reset();
 
 }
 
