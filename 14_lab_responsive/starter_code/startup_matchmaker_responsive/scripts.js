77b7098420f23cf6b1b9d3f732920438b849aadc
mattk045 <kennedy.mjh@gmail.com>
2015-11-24 20:24:37 -0500

    week 7 follow ups

---
+++ 14_lab_responsive/starter_code/startup_matchmaker_responsive/scripts.js
@@ -0,0 +1,28 @@
+jQuery( document ).ready(function() {
+
+
+
+
+//$(window).resize(function(){     
+
+  //     if ($('header').width() >= 701 ){
+
+              // is mobile device
+
+    //   }
+
+
+
+
+function iHaveBeenClicked(){
+
+$('.horizontal-nav li').toggle();
+
+}
+
+
+$('.hamburger').click(iHaveBeenClicked);
+
+
+
+});
